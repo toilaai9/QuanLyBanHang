2b6acffcb54b3e37de7bcce08a4671913e516450
toilaai9 <dotrongthi9@gmail.com>
2019-12-19 14:16:58 +0700

    thêm phan load theo loai

--- src/main/java/spring/qlbh/QUANLYBANHANG/dao/impl/HangDAOImpl.java
+++ src/main/java/spring/qlbh/QUANLYBANHANG/dao/impl/HangDAOImpl.java
@@ -11,6 +11,7 @@ import spring.qlbh.QUANLYBANHANG.dao.HangDAO;
 import spring.qlbh.QUANLYBANHANG.entity.Hang;
 import spring.qlbh.QUANLYBANHANG.model.HangInfo;
 
+
 public class HangDAOImpl implements HangDAO {
 	@Autowired
 	private SessionFactory sessionfactory;
@@ -18,17 +19,18 @@ public class HangDAOImpl implements HangDAO {
 	public List<HangInfo> loadHang() {
 		Session session = sessionfactory.getCurrentSession();
 		String sql = " select new  " + HangInfo.class.getName()
-					+" ( h.maHang, h.tenHang, h.donGia, h.imageLink, h.vAT, h.maLoai, h.nhaSX, h.ngaySX, h.tGBaoHanh, h.tTThem) " // các trường trong đối tượng HangInfo
-					+" from " + Hang.class.getName() + " h "; // select bàng Hibernate thì nó trả về 1 đối tượng
+					+"(h.maHang, h.tenHang, h.donGia, h.imageLink, h.vAT, h.maLoai, h.nhaSX, h.ngaySX, h.tGBaoHanh, h.tTThem)" // các trường trong đối tượng HangInfo
+					+" from " + Hang.class.getName() + " h "; // để ý chỗ này, chỗ này phải cách ra, chứ k nó lỗi
 		Query query = session.createQuery(sql);
 		return query.list();
 	}
+	//load hàng theo loại
 	@Override
 	public List<HangInfo> loadHangTheoLoai(int maLoai){
 		Session session = sessionfactory.getCurrentSession();
 		String sql = " select new  " + HangInfo.class.getName()
 					+" ( h.maHang, h.tenHang, h.donGia, h.imageLink, h.vAT, h.maLoai, h.nhaSX, h.ngaySX, h.tGBaoHanh, h.tTThem) " // các trường trong đối tượng HangInfo
-					+" from " + Hang.class.getName() + " h "+"where h.maLoai=maLoai"; // select bàng Hibernate thì nó trả về 1 đối tượng
+					+" from " + Hang.class.getName() + " h "+"where h.maLoai=: maLoai "; // select bàng Hibernate thì nó trả về 1 đối tượng
 		Query query = session.createQuery(sql);
 		query.setParameter("maLoai", maLoai);
 		return query.list();
